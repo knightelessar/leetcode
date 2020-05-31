30167c91e0d813b64055525c0ce7ba1ea79d15e1
knightelessar <knightelessar@gmail.com>
2020-05-31 12:52:21 +0800

    add avoiding revist visited nodes logic
    DFS debug logic and bug origin
    
    remove debug message and fix bug
    
    notes on the failed test case after upgrading to DFS
    
    annotate DFS solution

--- 20200530/CourseSchedule.cpp
+++ 20200530/CourseSchedule.cpp
@@ -1,4 +1,5 @@
 class Solution {
+    // DFS solution based on O(V^2 + E) backtrack
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         // build directed graph as adjacency list in a dictionary
@@ -18,12 +19,14 @@ public:
         }
 
         bool path[numCourses];
+        bool checked[numCourses];
         for (int i {0}; i < numCourses; ++i) {
             path[i] = 0;
+            checked[i] = 0;
         }
         // check all the course nodes see if start from 1 we can finish
         for (int course{0}; course < numCourses; ++course) {
-            if (isCyclic(course, courseDict, path)) {
+            if (isCyclic(course, courseDict, checked, path)) {
                 return false;
             }
         }
@@ -33,8 +36,22 @@ public:
     /* 
      * using breadcrumb in the path, start from a course node, 
      * incrementally follow the path by backtracking
+     * enhanced by avoiding revisiting checked nodes, so post order
+     * child 1 -> child 2 -> ... -> self node visiting will not revist
+     * checked nodes
+     * Failed candidate is in the sense of cyclic, returning true.
+     * Good candidate is one node with acyclic sub graph, returning false.
      */
-    bool isCyclic(int course, unordered_map<int, vector<int>>& courseDict, bool path[]) {
+    bool isCyclic(int course,
+                  unordered_map<int, vector<int>>& courseDict,
+                  bool checked[],
+                  bool path[]) {
+        
+        // if checked the node has no cyclic sub nodes, it is a good candidate
+        if (checked[course] == true) {
+            return false;
+        }
+        
         // failed candidate solution criteron to start backtrack (abondon the invalid)
         if (path[course] == true) {
             return true;
@@ -51,16 +68,22 @@ public:
         bool hasRevisited = false;
         // build all solutions from the initial course node incrementally
         for (int nextCourse: courseDict[course]) {
-            hasRevisited = isCyclic(nextCourse, courseDict, path);
+            hasRevisited = isCyclic(nextCourse, courseDict, checked, path);
             // stop (backtrack) when a node is revisited
-            if (path[nextCourse] == true) {
+            if (hasRevisited == true) {
                 break;
             }
         }
         
         // clear the breadcrumb
         path[course] = false;
-        
+
+        // we have not revisted any sub graph child node before processing self
+        // in post order DFS
+        // thus self node is checked
+        // as no sub graph is cyclic
+        checked[course] = true;
+
         return hasRevisited;
     }
 };
