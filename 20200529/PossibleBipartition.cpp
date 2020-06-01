448aab46fb81b05c036f7b9dc8dc09f8cc0e83e2
knightelessar <knightelessar@gmail.com>
2020-06-02 00:56:38 +0800

    convert raw pointer to smart pointer
    remove debug messages
    
    convert raw pointer to smart pointer

--- 20200529/PossibleBipartition.cpp
+++ 20200529/PossibleBipartition.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
-    vector<int>** graph;
+    shared_ptr<shared_ptr<vector<int>>> graph;
     unordered_map<int, int> colorMask;
 
     bool possibleBipartition(int N, vector<vector<int>>& dislikes) {
         // Initialize graph of people nodes
-        graph = new vector<int>*[N+1];
+        graph = shared_ptr<shared_ptr<vector<int>>[]>(new shared_ptr<vector<int>>[N+1]);
         for (int i = 1; i <= N; ++i) {
-            graph[i] = new vector<int>;
+            graph[i] = shared_ptr<vector<int>>(new vector<int>);
         }
         
         // Draw the edges between the nodes
@@ -20,7 +20,6 @@ public:
         for (int node {1}; node <= N; ++node) {
             if (colorMask.find(node) == colorMask.end()
                 && !paintFirstNodeBlueAndItsNeighbourWithAlternateColorDFS(node, 0)) {
-                cout << "Initial blue node: " << node << endl;
                 return false;
             }
         }
@@ -31,7 +30,6 @@ public:
         if (colorMask.find(node) != colorMask.end()) {
             // Color mask does already contain the node
             if (node == 2) {
-                cout << "Initial chain node 2 has color: " << colorMask[2] << ", new color: " << c << endl;
             }
             return colorMask[node] == c;
         }
@@ -39,10 +37,8 @@ public:
         
         for (int nei: *graph[node]) {
             if (nei == 41 && node == 2) {
-                cout << "Chain initial node 2 with indirect Nei node 41: " << endl;
             }
             if(!paintFirstNodeBlueAndItsNeighbourWithAlternateColorDFS(nei, c ^ 1)) {
-                cout << "Chain initial node: " << node << ", " << "new color: " << c << ", " << "current nei node: " << nei << ", current color: " << colorMask[nei] << ", new color: " << (c ^ 1) << endl;
                 return false;
             }
         }
