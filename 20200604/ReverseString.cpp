3abd839c3fd137ea58012bbed105fce1ae0703b6
knightelessar <knightelessar@gmail.com>
2020-06-04 15:49:05 +0800

    add empty string checking

--- 20200604/ReverseString.cpp
+++ 20200604/ReverseString.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     void reverseString(vector<char>& s) {
+        if (s.empty()) {
+            return;
+        }
         auto i = begin(s);
         auto j = end(s) - 1;
         while (i < j) {
