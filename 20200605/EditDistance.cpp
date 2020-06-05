43aee8c4e426f73c826675f7d12b8978b4b76557
knightelessar <knightelessar@gmail.com>
2020-06-05 13:46:36 +0800

    fixed bug
    remove debug msg
    Revert "add debug msg"
    
    This reverts commit 9103ef50c678390f429e629e45fb31550d4be735.

--- 20200605/EditDistance.cpp
+++ 20200605/EditDistance.cpp
@@ -19,34 +19,15 @@ public:
         for (int i{1}; i < len1 + 1; ++i) {
             // word2[1..j]
             for (int j{1}; j < len2 + 1; ++j) {
-                cout << "i: " << i << ", j:" << j << endl;
-                cout << "Working on : " << word1.substr(0, i) << " and ";
-                cout << word2.substr(0, j) << endl;
-                
                 // word1[1..(i-1)] against word2[1..j]
                 int distParW1 = dist[i-1][j];
-                cout << "distParW1 ";
-                cout << word1.substr(0, i-1) << " and " << word2.substr(0, j);
-                cout << " is "<< distParW1 << endl;
-
                 // word1[1..i] against word2[1..(j-1)]
                 int distParW2 = dist[i][j-1];
-                cout << "distParW1 ";
-                cout << word1.substr(0, i) << " and " << word2.substr(0, j-1);
-                cout << " is "<< distParW1 << endl;
-
                 // word1[1..(i-1)] against word2[1..(j-1)]
                 int distParW1ParW2 = dist[i-1][j-1];
-                cout << "distParW1ParW2 ";
-                cout << word1.substr(0, i-1) << " and " << word2.substr(0, j-1);
-                cout << " is " << distParW1ParW2 << endl;
-                
-                bool isLastCharSame = (word1[i] == word2[j]); // <== bug is here
-                cout << word1.substr(0, i) << " and " << word2.substr(0, j);
-                cout << " has " << (isLastCharSame ? "same" : "different") << " last char ";
-                cout << word1[i-1] << " and " << word2[j-1] << endl;
+                bool isLastCharSame = (word1[i-1] == word2[j-1]);
                 dist[i][j] = min(min(distParW1 + 1, distParW2 + 1),
-                                isLastCharSame ? distParW1ParW2 : (distParW1ParW2 + 1));
+                                isLastCharSame ? distParW1ParW2 : distParW1ParW2 + 1);
             }
         }
         
