d6bc76d5e90258db8dd32b2cd98a74e99262ae62
knightelessar <knightelessar@gmail.com>
2020-06-05 23:03:52 +0800

    binary search: mind the >= on line 19

--- 20200605/RandomPickWithWeight.cpp
+++ 20200605/RandomPickWithWeight.cpp
@@ -10,26 +10,29 @@ public:
     }
     
     int pickIndex() {
-        int myRand = getRand(this->bounds.back());
+        int myRand = getRand(bounds.back());
         // int myRand = rand() % bounds.back();
-        int index = static_cast<int>(bounds.size() - 1);
-        for (size_t i{0}; i < this->bounds.size(); ++i) {
-            if (myRand < this->bounds[i]) {
-                index = static_cast<int>(i);
-                break;
+        size_t right = bounds.size();
+        size_t left = 0;
+        while (left < right) {
+            size_t mid = left + (right - left) / 2;
+            if (myRand >= bounds[mid]) {
+                left = mid + 1;
+            }
+            else {
+                right = mid;
             }
         }
-        return index;
+
+        return left;
     }
     
     int getRand(int modulo) {
         if (modulo == 0) {
-            cout << "modulo = 0" << endl;
             return 0;
         }
         while (true) {
             int x = rand();
-            cout << RAND_MAX << endl;
             if (x < RAND_MAX - RAND_MAX % modulo) {
                 return x % modulo;
             }
