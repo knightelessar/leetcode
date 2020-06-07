004a04aea23ca370aa2783dbf0346f545f9b9064
knightelessar <knightelessar@gmail.com>
2020-06-07 16:04:31 +0800

    time limit exceeded solution

---
+++ CoinChange2.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
+        // Sort the coins in ascending order
+        sort(begin(coins), end(coins), greater());
+        return nValidChange(amount, coins, 0, coins.size() - 1);
+    }
+    
+    int nValidChange(int rest, vector<int>& coins, size_t coinType, size_t lastCoinType) {
+        if (coinType > lastCoinType || coins.empty()) {
+            // Out of coin type
+            return rest == 0 ? 1 : 0;
+        }
+        else if (coinType == lastCoinType) {
+            // Last (smallest coin type)
+            return rest % coins[coinType] == 0 ? 1 : 0;
+        }
+        
+        int nWay = 0;
+        int maxCoinType = rest / coins[coinType];
+        
+        for (int i = maxCoinType; i >= 0; --i) {
+            nWay = nWay += nValidChange(rest - i * coins[coinType],
+                                        coins,
+                                        coinType + 1,
+                                        lastCoinType);
+        }
+        return nWay;
+    }
+};
