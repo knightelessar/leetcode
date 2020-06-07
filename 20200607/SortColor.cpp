96604b77f3ae8b552798970bf77f622102274773
knightelessar <knightelessar@gmail.com>
2020-06-07 11:24:06 +0800

    fix unprocessed 2 logic

--- 20200607/SortColor.cpp
+++ 20200607/SortColor.cpp
@@ -5,15 +5,18 @@ public:
         pCurr = 0;
         p0r = 0;
         p2l = nums.size() - 1;
-        for (pCurr = 0; pCurr < p2l; ++pCurr) {
+        while (pCurr < p2l) {
             if (nums[pCurr] == 0) {
                 swap(nums[pCurr], nums[p0r]);
                 ++p0r;
+                ++pCurr;
             }
             else if (nums[pCurr] == 2) {
                 swap(nums[pCurr], nums[p2l]);
-                --p2l; // <= bug here, the number 2 is swapped to pCurr, 
-                       // and we left it unprocessed in future loops.
+                --p2l;
+            }
+            else {
+                ++pCurr;
             }
         }
     }
