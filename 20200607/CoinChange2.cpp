6592401b30d31964365ddc2a8d23a777cca024fe
knightelessar <knightelessar@gmail.com>
2020-06-07 21:54:28 +0800

    debug backtrack
    remove driver and comments

--- 20200607/CoinChange2.cpp
+++ 20200607/CoinChange2.cpp
@@ -1,12 +1,15 @@
 class Solution
 {
+    int counter;
 public:
     unordered_map<int, unordered_map<int, int>> ways; // key: rest, value: pair<currCoin, nWays>
     int change(int amount, vector<int> &coins)
     {
         // Sort the coins in ascending order
-        sort(begin(coins), end(coins), greater());
-        return nValidChange(amount, coins, 0, coins.size() - 1);
+        sort(begin(coins), end(coins));
+        vector<vector<int>> ans;
+        bc(coins, amount, 0, ans);
+        return counter;
     }
 
     int nValidChange(int rest, vector<int> &coins, size_t coinType, size_t lastCoinType)
@@ -43,5 +46,39 @@ public:
             ways[rest][coinType] = nWay;
         }
         return nWay;
+    }
+
+    /* 
+     * Mincan's back track routine
+     * For number of valid combinations, we do not need to save the path.
+     */
+    void bc(vector<int>& coins, int target, int curIdx, vector<vector<int>>& ans, vector<int>&& path = {})
+    {
+        if (target == 0)
+        {
+            ans.push_back(path);
+            counter++;
+            for (auto a: path) {
+                cout << " " << a;
+            }
+            cout << ": counter = " << counter << endl;
+            return;
+        }
+
+        for (int i = curIdx; i < coins.size(); i++)
+        {
+            path.push_back(coins[i]);
+            for (auto a: path) {
+                if (a > 0)
+                cout << " " << a;
+            }
+            cout << endl;
+            cout << ": target = " << target << ", coins[" << i << "] = " << coins[i] << endl;
+            if (target - coins[i] < 0) {
+                continue;
+            }
+            bc(coins, target - coins[i], i, ans, move(path));
+            path.pop_back();
+        }
     }
 };
