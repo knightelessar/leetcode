97119007f89533f76b58d18ac5212cf2d4f167da
knightelessar <knightelessar@gmail.com>
2020-06-08 12:32:58 +0800

    problem statement
    pseudo code
    
    naive time calculation attempt
    
    fix typo
    
    input end time as tm structure
    
    ctime way time calculation
    
    ctime way mktime updated time
    
    correct year
    
    validate input time by mktime

---
+++ 20200608/TimeDiff.cpp
@@ -0,0 +1,26 @@
+// Now is 12:30 pm, my timer is 1h40m, when did I start the timer?
+// Use C++ ctime library to output the start time in hh:mm am format.
+
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+int main() {
+    tm endTimeSpec;
+    endTimeSpec.tm_year = 2020 - 1900; // year since 1900
+    endTimeSpec.tm_mon = 5; // months since Jan [0, 11]
+    endTimeSpec.tm_mday = 8; // day of the month [1, 31]
+    endTimeSpec.tm_hour = 12; // hours since midnight [0, 23]
+    endTimeSpec.tm_min = 30; // minutes after the hour [0, 59]
+    endTimeSpec.tm_sec = 0; // seconds after the minute [0, 60]
+    mktime(&endTimeSpec);
+    cout << "end time: \t" << asctime(&endTimeSpec) << endl;
+
+    tm startTime = tm(endTimeSpec);
+    // back time up by 1h40m
+    startTime.tm_hour -= 1;
+    startTime.tm_min -= 40;
+    mktime(&startTime);
+    cout << "start time: \t" << asctime(&startTime) << endl;
+}
+
