6a4ec059d3d60007791e175be52ffff25bed0e6b
knightelessar <knightelessar@gmail.com>
2020-06-10 21:59:56 +0800

    add boundary cases

--- 20200610/SearchInsertPosition.cpp
+++ 20200610/SearchInsertPosition.cpp
@@ -1,6 +1,14 @@
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
+        if (target < nums.front())
+        {
+            return 0;
+        }
+        if (target > nums.back())
+        {
+            return nums.size();
+        }
         // Binary search
         size_t left{0};
         size_t right{nums.size() - 1};
