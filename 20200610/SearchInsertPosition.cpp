a77b88584dcf46d4a564c89c8908ffa1b0f0196d
knightelessar <knightelessar@gmail.com>
2020-06-10 22:00:17 +0800

    remove debug messages

--- 20200610/SearchInsertPosition.cpp
+++ 20200610/SearchInsertPosition.cpp
@@ -14,18 +14,12 @@ public:
         size_t right{nums.size() - 1};
         size_t mid{0};
         while (left < right) {
-            cout << "left = " << left << ", right = " << right << endl;
             mid = (left + right) / 2;
-            cout << "mid = " << mid << endl;
             if (target <= nums[mid]) {
-                cout << "target = " << target << " <= " << "nums[" << mid << "] = " << nums[mid] << endl;
                 right = mid;
-                cout << "right <- mid = " << mid << endl;
             }
             else {
-                cout << "target = " << target << " > " << "nums[" << mid << "] = " << nums[mid] << endl;
                 left = mid + 1;
-                cout << "left <- mid + 1 = " << mid+1 << endl;
             }
         }
         return left;
